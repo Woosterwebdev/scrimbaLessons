5efc56c16250df34dbe0b54aefc909ffc2d1a053
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-02-02 09:28:35 -0700

    Add files via upload

--- Module 3 Making Websites Interactive/Build a passenger counter app/09. Create the save feature/index.js
+++ Module 3 Making Websites Interactive/Build a passenger counter app/09. Create the save feature/index.js
@@ -1,12 +1,21 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    // Change this to use the plus equal technique you've learned
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count // use .textContent as .innerText removes human unreadables 
 }
 
 function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    
+    let saveSet = count + " - "
+    
+    saveEl.textContent += saveSet // use .textContent as .innerText removes human unreadables
+    
     console.log(count)
 }

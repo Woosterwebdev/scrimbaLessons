8e0844ecf19bb753e2f1d5cc0697d198a4fc0a1a
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-02-14 13:01:20 -0700

    Add files via upload

--- Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a Chrome Extension!/02. Add onclick/index.js
+++ Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a Chrome Extension!/02. Add onclick/index.js
@@ -1,4 +1,17 @@
+
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-function saveLead() {
+// Grab the box from the DOM and store it in a variable
+// Add a click event listener to the box 
+// Log out "I want to open the box!" when it's clicked
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let myLeads = []
+// If possible, use const. If not, use let.
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
-}
+})
---
+++ Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a Chrome Extension!/03. Push to the myLeads array/index.js
@@ -0,0 +1,18 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
+
+inputBtn.addEventListener("click", function() {
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+// Log out the items in the myLeads array using a for loop 
+for (i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}

e988d9d53e6c9d54723a6cf8833fa7f4ecf23b41
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-02-23 11:11:38 -0700

    Add files via upload

---
+++ Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a mobile app/02. Adding styling/index.js
@@ -0,0 +1,8 @@
+const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    console.log(inputValue)
+})
---
+++ Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a mobile app/03. Adding Firebase/index.js
@@ -0,0 +1,23 @@
+// Challenge: Import 'initializeApp' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// Challenge: Import 'getDatabase' from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://add-to-cart-1562a-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB, inputValue)
+    
+    console.log(inputValue)
+})

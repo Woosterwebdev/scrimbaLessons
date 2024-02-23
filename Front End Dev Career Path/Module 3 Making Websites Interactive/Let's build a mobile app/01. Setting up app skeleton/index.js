6585ff3ca730d360ff0b15ea6180e212c09fa094
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-02-23 09:12:26 -0700

    Add files via upload

--- Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a mobile app/01. Setting up app skeleton/index.js
+++ Front End Dev Career Path/Module 3 Making Websites Interactive/Let's build a mobile app/01. Setting up app skeleton/index.js
@@ -2,3 +2,12 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+
+const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    console.log(inputValue)
+})

7cc88223c8c99a267a2a2d7c1493de28bf122a6b
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-18 14:55:46 -0700

    Continued lessons

--- Front End Dev Career Path/Module 5 Essential JavaScript concepts/Twimba Twitter clone/02. Add text area/index.js
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/Twimba Twitter clone/02. Add text area/index.js
@@ -1,3 +1,13 @@
-import { tweetsData } from "./data";
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-console.log(tweetsData)
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+/*
+Challenge
+1. Have the eventListener's function
+   log out the contents of the textarea
+   when the Tweet button is clicked.
+*/
+})

98b1d4e242fbc6db87c97876206b9a0b550a8d84
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-20 11:52:30 -0700

    Lessons continued

--- Front End Dev Career Path/Module 5 Essential JavaScript concepts/Twimba Twitter clone/12. Retweet a tweet/index.js
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/Twimba Twitter clone/12. Retweet a tweet/index.js
@@ -9,15 +9,7 @@ tweetBtn.addEventListener('click', function(){
 document.addEventListener('click', function(e){
     if ('like', e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
-    } else if(e.target.dataset.retweet){
-        handleRetweetClick(e.target.dataset.retweet)
     }
-/*
-Challenge:
-1. Make this eventListener call "handleRetweetClick" 
-   when the retweet icon is clicked, passing in the
-   uuid from that tweet.  
-*/
 })
     
 function handleLikeClick(tweetId){ 
@@ -25,34 +17,20 @@ function handleLikeClick(tweetId){
         return tweet.uuid === tweetId
     })[0]
 
+    /*
+    Challenge:
+    1. Delete the two lines of code marked below and
+    replace them with just one line of code outside 
+    of the if else.
+    Hint: Google the logical NOT operator (!)
+    */ 
+    
     if (targetTweetObj.isLiked){
     targetTweetObj.likes--
     } 
     else {targetTweetObj.likes++
     }
     targetTweetObj.isLiked = !targetTweetObj.isLiked
-    render()
-}
-
-function handleRetweetClick(tweetId){
-    /*
-    Challenge:
-    2. Find the retweeted tweet's object in tweetsData 
-       and save it to a const.
-    3. Increment or decrement the retweet count of the 
-       tweet and flip its isRetweeted boolean.
-    4. Call the render function.  
-    */
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-    
-    if (targetTweetObj.isRetweeted){
-        targetTweetObj.retweets--
-    } else {
-        targetTweetObj.retweets++
-    }
-    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
     render()
 }
 

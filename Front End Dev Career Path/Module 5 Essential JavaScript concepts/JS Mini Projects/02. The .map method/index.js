79fd93e0d3198b9d6c8fdbf6dfd60bccdc479b05
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-21 13:58:33 -0700

    Continued lessons

--- Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/02. The .map method/index.js
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/02. The .map method/index.js
@@ -7,24 +7,43 @@ Challenge
    ⚠️ Don't worry about the commas for now.
 */
 
-const playlistHtml = []
+// const playlistHtml = []
 
-for (let i = 0; i < playlistArr.length; i++) {
-    playlistHtml.push(
-`<section class="card">
+// for (let i = 0; i < playlistArr.length; i++) {
+//     playlistHtml.push(
+// `<section class="card">
+//     <div class="card-start">
+//         <img src="/images/${playlistArr[i].albumArt}">
+//     </div>
+//         <div class="card-mid">
+//             <h4 class="card-title">${playlistArr[i].title}</h4>
+//             <p class="card-artist">${playlistArr[i].artist}</p>
+//         </div>
+//     <div class="card-end">
+//         <p class="card-menu">...</p>
+//     </div>
+// </section>
+//     `
+//     )
+// }
+
+
+const playlistHtml = playlistArr.map(function(song){
+    return `
+<section class="card">
     <div class="card-start">
-        <img src="/images/${playlistArr[i].albumArt}">
+        <img src="/images/${song.albumArt}">
     </div>
         <div class="card-mid">
-            <h4 class="card-title">${playlistArr[i].title}</h4>
-            <p class="card-artist">${playlistArr[i].artist}</p>
+            <h4 class="card-title">${song.title}</h4>
+            <p class="card-artist">${song.artist}</p>
         </div>
     <div class="card-end">
         <p class="card-menu">...</p>
     </div>
 </section>
     `
-    )
-}
+    
+})
 
 document.getElementById('container').innerHTML = playlistHtml

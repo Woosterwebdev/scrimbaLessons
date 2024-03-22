c0e8afb9877e16617e5b6f4d437cd17f29df9ded
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-22 13:45:14 -0700

    Continued lessons

--- Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/09. The .reduce() method/index.js
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/09. The .reduce() method/index.js
@@ -1,1 +1,24 @@
-const rainJanuaryByWeek = [ 10, 20, 0, 122 ]
+const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73]
+
+/*
+Challenge
+1. Use the .reduce() method to find the total of all of the students grades.
+2. Do some simple maths to log out the class average. 
+*/
+// const scoresTotal = grades.reduce(function (total, currentElement){
+//     return total + currentElement
+// })
+
+// console.log(`The class average is ${scoresTotal/grades.length}`)
+
+/*
+Challenge:
+Convert this anonymous function to an arrow function. 
+Be as concise as you can.
+*/
+
+
+const totalGrades = grades.reduce((total, currentGrade) => total + currentGrade)
+
+
+console.log(`The class average is ${totalGrades/grades.length}`)

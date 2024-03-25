6f1d1d9a4ebbc87a4c0ecb79e94ccc06659793a8
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-25 11:38:46 -0700

    Continued lessons

--- Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/12. Ternary Operator Challenge/index.js
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/12. Ternary Operator Challenge/index.js
@@ -14,6 +14,19 @@ Challenge
 //     message = 'Wrong!'
 // }
 
-const message = playerGuess === correctAnswer ? 'Correct!' : 'Wrong!'
+// const message = playerGuess === correctAnswer ? 'Correct!' : 'Wrong!'
+
+// console.log(message)
+
+/*
+Challenge
+1. Now improve the functionality of this code by 
+   letting the player know if their guess was too high, 
+   too low, or exactly right.
+*/
+
+const message = playerGuess < correctAnswer ? 'Too low!' 
+: playerGuess > correctAnswer ? 'Too high!' 
+: 'Correct!'
 
 console.log(message)

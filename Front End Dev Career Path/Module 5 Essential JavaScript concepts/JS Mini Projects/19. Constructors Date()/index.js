66e02fcc3f6ef6433b121c91adf4c9f1531466ef
Christopher Wooster <woosterwebdev@gmail.com>
2024-03-27 08:51:36 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Module 5 Essential JavaScript concepts/JS Mini Projects/19. Constructors Date()/index.js
@@ -0,0 +1,11 @@
+// const dateSnapshot = new Date()
+// console.log(`Copyright ${dateSnapshot.toString()}`)
+
+/*
+Challenge:
+1. Search online to find out how we can get just the year 
+   using the Date() constructor and update the console.log above.
+*/
+
+const dateSnapshot = new Date()
+console.log(`Copyright ${dateSnapshot.getFullYear().toString()}`)

0a6584ad819711796c646e1d5f35c60f9dc2fed5
Christopher Wooster <woosterwebdev@gmail.com>
2024-05-06 10:59:35 -0700

    Continued lessons

--- Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/09. BlogSpace - Post Refactor/index.js
+++ Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/09. BlogSpace - Post Refactor/index.js
@@ -1,4 +1,32 @@
-document.getElementById('blog-form').addEventListener("submit", function(e) {
+/**
+    * Challenge: Use our new renderPosts function to clean up this code.
+    * 
+    * Don't forget to update the postsArray variable first!
+*/
+
+let blogsArr = []
+const blogForm = document.getElementById('blog-form')
+
+function renderBlogs() {
+    let html = ""
+        for (let blog of blogsArr) {
+            html += `
+            <h3>${blog.title}</h3>
+            <p>${blog.body}</p>
+            <hr />
+        `
+        }
+    document.getElementById('blog-list').innerHTML = html
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        blogsArr = data.slice(0, 5)
+        renderBlogs()
+    })
+
+blogForm.addEventListener("submit", function(e) {
     e.preventDefault()
     const blogTitle = document.getElementById('blog-title').value
     const blogBody = document.getElementById('blog-body').value
@@ -17,27 +45,11 @@ document.getElementById('blog-form').addEventListener("submit", function(e) {
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
         .then(blog => {
-            document.getElementById('blog-list').innerHTML = `
-                <h3>${blog.title}</h3>
-                <p>${blog.body}</p>
-                <hr />
-                ${document.getElementById('blog-list').innerHTML}
-            `
+            blogsArr.unshift(blog)
+            renderBlogs()
+            /**
+             * Challenge: clear the form out!
+             */
+            blogForm.reset()
         })
 })
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const blogsArr = data.slice(0, 5)
-        let html = ""
-        for (let blog of blogsArr) {
-            html += `
-            <h3>${blog.title}</h3>
-            <p>${blog.body}</p>
-            <hr />
-        `
-        }
-        document.getElementById('blog-list').innerHTML = html
-    })
-
---
+++ Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/10. REST/quiz.md
@@ -0,0 +1,8 @@
+1. How would you describe what REST is to your non-technical friend?
+   A server that has a standardized response for a range of clients
+
+2. What does a RESTful API usually return in response to incoming requests?
+   JSON
+
+3. What kind of client devices can make use of a RESTful API?
+   All of them. Computers, phones, smart watches, stoves, washers, dryers, smart fridges, etc

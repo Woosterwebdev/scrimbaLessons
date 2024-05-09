0c75962b2b117e99706385a1e4f1f82a176e9845
Christopher Wooster <woosterwebdev@gmail.com>
2024-05-09 10:50:24 -0700

    Continued lessons

--- Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/16. Color Scheme Picker/index.js
+++ Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/16. Color Scheme Picker/index.js
@@ -1,3 +1,18 @@
 // Base URL - https://www.thecolorapi.com 
 // Scheme endpoint - /scheme{?hex,rgb,hsl,cmyk,format,mode,count}
 
+const seedColor = document.getElementById('seed-color')
+const schemeSelect = document.getElementById('scheme-select').value
+const colorForm = document.getElementById('color-form')
+
+colorForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    fetchColors()
+})
+
+function fetchColors() {
+    console.log(schemeSelect)
+    // const color = seedColor.value.slice(1)
+    // const mode =  schemeSelect
+    // fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
+}

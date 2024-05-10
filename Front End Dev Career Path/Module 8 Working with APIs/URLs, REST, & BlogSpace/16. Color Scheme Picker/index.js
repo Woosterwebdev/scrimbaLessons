805661ee6b30dedf5448ef5e530fd7ca33399e6d
Christopher Wooster <woosterwebdev@gmail.com>
2024-05-10 09:21:07 -0700

    Continued lessons

--- Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/16. Color Scheme Picker/index.js
+++ Front End Dev Career Path/Module 8 Working with APIs/URLs, REST, & BlogSpace/16. Color Scheme Picker/index.js
@@ -2,17 +2,29 @@
 // Scheme endpoint - /scheme{?hex,rgb,hsl,cmyk,format,mode,count}
 
 const seedColor = document.getElementById('seed-color')
-const schemeSelect = document.getElementById('scheme-select').value
+const schemeSelect = document.getElementById('scheme-select')
 const colorForm = document.getElementById('color-form')
+
+let schemeArr = []
 
 colorForm.addEventListener("submit", function(e) {
     e.preventDefault()
     fetchColors()
+    renderResults()
 })
 
 function fetchColors() {
-    console.log(schemeSelect)
-    // const color = seedColor.value.slice(1)
-    // const mode =  schemeSelect
-    // fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
+    const color = seedColor.value.slice(1)
+    const mode =  schemeSelect.value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
+        .then(res => res.json())
+        .then(data => data.colors.forEach((color) => schemeArr.push(color.hex.value)))
+}
+
+function rendeResults() {
+    schemeArr.forEach((color) => {
+        document.getElementById('color-preview').innerHTML += `
+            <div class="width-100"></div>
+        `
+    })
 }

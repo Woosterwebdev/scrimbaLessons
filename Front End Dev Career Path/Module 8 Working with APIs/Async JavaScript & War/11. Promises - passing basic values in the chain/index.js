b36eab16a5520a67a19508db617692468fb9bcf1
Christopher Wooster <woosterwebdev@gmail.com>
2024-05-14 13:26:41 -0700

    Continued lessons

--- Front End Dev Career Path/Module 8 Working with APIs/Async JavaScript & War/11. Promises - passing basic values in the chain/index.js
+++ Front End Dev Career Path/Module 8 Working with APIs/Async JavaScript & War/11. Promises - passing basic values in the chain/index.js
@@ -8,10 +8,22 @@
  * parameter if you returned something totally different??
  */
 
+/**
+ * Challenge:
+ * 
+ * pass the string "World" down to a 3rd .then() block
+ * and log it to the console inside the body of this new
+ * 3rd .then() block
+ */
+
 fetch("https://apis.scrimba.com/bored/api/activity")
     .then(function(res) {
-        return ___
+        return "Hello"
     })
-    .then(function(___) {
-        
+    .then(function(whatever) {
+        console.log(whatever)
+        return "World"
+    })
+    .then(function(data) {
+        console.log(data)
     })

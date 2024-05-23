b3017fad2f40479d35a458cc93e32016505edff0
Christopher Wooster <woosterwebdev@gmail.com>
2024-05-23 10:47:23 -0700

    Continued project

---
+++ Front End Dev Career Path/Module 8 Working with APIs/Async JavaScript & War/Solo Project Movie Watchlist/mywatchlist.js
@@ -0,0 +1,30 @@
+
+/*
+<div id="search-results" class="populated-results">
+    <div class="movie-container">
+        <img class="poster" src="images/poster.jpg" />
+        <div class="movie-details">
+          <div class="details1">
+            <p class="movie-title">Blade Runner</p>
+            <i class="fa-solid fa-star" style="color: #fec654"></i>
+            <p class="imdb-rating">8.1</p>
+          </div>
+          <div class="details2">
+            <p>116 min</p>
+            <p>Action, Drama, Sci-Fi</p>
+            <p class="add-to-watchlist">
+              <i class="fa-solid fa-circle-plus" style="color: #ffffff"></i
+              >Watchlist
+            </p>
+          </div>
+          <div class="details3">
+            <p>
+              A blade runner must pursue and terminate four replicants who stole
+              a ship in space and have returned to Earth to find their creator.
+            </p>
+          </div>
+        </div>
+    </div>
+    <hr />
+</div>
+*/

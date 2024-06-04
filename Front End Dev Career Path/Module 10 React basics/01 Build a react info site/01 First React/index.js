715f6716bbf076c2697712725ed22857806e12a8
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-04 09:26:05 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Module 10 React basics/01 Build a react info site/01 First React/index.js
@@ -0,0 +1,3 @@
+// Spend a minute memorizing the line below
+// Re-write the line of code as best you can from memory
+ReactDOM.render(<h1>First line of React!</h1>, document.getElementById('root'))

f78afb66bc55c7a6e10c481ea1854c55311e3aa5
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-07 12:54:08 -0700

    Continued lessons

--- Front End Dev Career Path/Module 10 React basics/01 Build a react info site/17 Solo project Digital Bussiness Card/components/Main.js
+++ Front End Dev Career Path/Module 10 React basics/01 Build a react info site/17 Solo project Digital Bussiness Card/components/Main.js
@@ -6,11 +6,11 @@ export default function Main() {
             <h1 className="name">Christopher Wooster</h1>
             <h2 className="title">Frontend Developer</h2>
             <div className="contact-container">
-                <a href="#"><img src="../images/emailIcon.png" alt="Email link" /></a>
-                <a href="#"><img src="../images/linkedInIcon.png" alt="LinkedIn link" /></a>
-                <a href="#"><img src="../images/githubIcon.png" alt="Github link" /></a>
+                <a href="mailto:woosterwebdev@gmail.com"><img src="../images/email.png" className="icon"/></a>
+                <a href="https://github.com/Woosterwebdev"><img src="../images/github.png" className="icon" /></a>
+                <a href="https://www.linkedin.com/in/chris-wooster/"><img src="../images/linkedin.png" className="icon" /></a>
             </div>
-            <h3>About</h3>
+            <h3 className="about">About</h3>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             <h3>Interests</h3>
             <p>3D printing, CAD, video games, custom computers, cats, new food adventures</p>

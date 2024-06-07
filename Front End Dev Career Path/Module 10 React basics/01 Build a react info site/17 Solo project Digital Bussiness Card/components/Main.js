b7349aa416781a7fd62cf121a95e43b5a92ce25e
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-07 10:20:19 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Module 10 React basics/01 Build a react info site/17 Solo project Digital Bussiness Card/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <div>
+            <h1>Christopher Wooster</h1>
+            <h2>Frontend Developer</h2>
+            <a><img src="../images/emailIcon.png" alt="Email link" /></a>
+            <a><img src="../images/linkedInIcon.png" alt="LinkedIn link" /></a>
+            <a><img src="../images/githubIcon.png" alt="Github link" /></a>
+            <h3>About</h3>
+            <p></p>
+            <h3>Interests</h3>
+            <p></p>
+        </div>
+    )
+}

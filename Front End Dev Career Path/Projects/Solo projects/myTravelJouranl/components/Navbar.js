649012a3ce619ef1ca273f5443a0795ef641bcfc
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-17 11:09:26 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Projects/Solo projects/myTravelJouranl/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/earth-icon.png" alt="Logo" className="nav--logo" />
+            <h1 className="nav--title">my travel journal.</h1>
+        </nav>
+    )
+}

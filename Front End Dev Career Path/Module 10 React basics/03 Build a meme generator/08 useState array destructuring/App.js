354139593525552f112afc20a047362f8cc7cc2c
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-21 08:43:00 -0700

    Continued lessons

--- Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/08 useState array destructuring/App.js
+++ Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/08 useState array destructuring/App.js
@@ -1,13 +1,34 @@
+// import React from "react"
+
+// export default function App() {
+//     const result = React.useState("Yes")
+//     console.log(result)
+//     return (
+//         <div className="state">
+//             <h1 className="state--title">Is state important to know?</h1>
+//             <div className="state--value">
+//                 <h1>{result[0]}</h1>
+//             </div>
+//         </div>
+//     )
+// }
+
+/* 
+Return array [Yes, f{}]
+With array destructuring Yes is assigned to the variable result
+See below for example
+*/
+
 import React from "react"
 
 export default function App() {
-    const result = React.useState("Yes")
+    const [result, func] = React.useState("Yes")
     console.log(result)
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
             <div className="state--value">
-                <h1>{result[0]}</h1>
+                <h1>{result}</h1>
             </div>
         </div>
     )

8c9354e43b26fdddb9eb3fac804c65ed820b844f
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-25 11:09:39 -0700

    Continued lessons

--- Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/18 Complex state updating state objects/App.js
+++ Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/18 Complex state updating state objects/App.js
@@ -12,7 +12,13 @@ export default function App() {
     let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
     
     function toggleFavorite() {
-        console.log("Toggle Favorite")
+        // Challenge: build it :)
+        setContact(prevContact => {
+            return {
+                ...prevContact,
+                isFavorite: !prevContact.isFavorite
+            }
+        })
     }
     
     return (

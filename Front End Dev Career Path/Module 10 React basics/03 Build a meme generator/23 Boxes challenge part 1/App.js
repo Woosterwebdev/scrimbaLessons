b3a60fe6eaf84729c8a8d988469e18938360a9c5
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-26 11:26:58 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/23 Boxes challenge part 1/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import boxes from "./boxes"
+
+export default function App() {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+    
+    const [squares, setSquares] = React.useState(boxes)
+    const squareElements = squares.map(square => (
+        <div className="box" key={square.id}></div>
+    ))
+                
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}

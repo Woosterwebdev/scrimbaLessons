92164396d112d8be203ff29fcd8afb039c1b913c
Christopher Wooster <woosterwebdev@gmail.com>
2024-06-28 11:46:32 -0700

    Continued lessons

---
+++ Front End Dev Career Path/Module 10 React basics/03 Build a meme generator/34 Watch for input changes in React/Form.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Form() {
+    const [firstName, setFirstName] = React.useState("")
+    
+    console.log(firstName)
+    
+    function handleChange(event) {
+        setFirstName(event.target.value)
+        /**
+         * Challenge: update the firstName state on every keystroke
+         */
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+            />
+        </form>
+    )
+}

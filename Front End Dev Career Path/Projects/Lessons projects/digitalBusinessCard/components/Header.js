6f24354e14814f934a7088e5ccb3073dba45ed52
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-03 16:14:16 -0700

    cleanup

--- Front End Dev Career Path/Projects/Lessons projects/digitalBusinessCard/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Header() {
-    return (
-        <img src="../images/chris.jpg" alt="Headshot of Christopher Wooster" className="headshot" />
-    )
-}
--- Front End Dev Career Path/Projects/Solo projects/my-portfolio/src/components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Nav() {
-    return (
-        <nav>
-            <h1 className="nav--title">Christopher Wooster</h1>
-        </nav>
-    )
-}

7d36439dcaa878883f937327c14d3d13e747b1a7
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-05 10:33:38 -0700

    Continued lessons

--- Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/01 Warm-up Add dark-light modes to ReactFacts site/App.js
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/01 Warm-up Add dark-light modes to ReactFacts site/App.js
@@ -3,10 +3,21 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+    
+    function toggle() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={darkMode}
+                toggleDarkMode={() => toggle()}            
+            />
+            <Main 
+                darkMode={darkMode}
+            />
         </div>
     )
 }

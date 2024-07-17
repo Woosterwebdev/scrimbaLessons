1910511a44ef7850fc6eb17af6d6fdcfd92df70f
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-17 12:14:03 -0700

    Importing firebase and testing useEffect

---
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/08 Local setup/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbSEa6T-nF0uxo5quFccBmIG3yTIXIBFk",
+  authDomain: "react-notes-app-8a4d7.firebaseapp.com",
+  projectId: "react-notes-app-8a4d7",
+  storageBucket: "react-notes-app-8a4d7.appspot.com",
+  messagingSenderId: "689147384042",
+  appId: "1:689147384042:web:e4b83274b80abaffa94793"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export const notesCollection = collection(db, "notes")

9e3787e7fdc4f2d7c9bdea2f1ec14527813bec37
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-18 12:30:21 -0700

    Tenzies array

--- Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/15 Tenzies Die component/src/App.jsx
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/15 Tenzies Die component/src/App.jsx
@@ -4,10 +4,17 @@ import Die from "./Die"
 /**
  * Challenge:
  * 
- * Write a function (allNewDice) that returns an array 
- * of 10 random numbers between 1-6 inclusive.
- * 
- * Log the array of numbers to the console for now
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
  */
 
 export default function App() {

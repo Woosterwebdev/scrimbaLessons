d704d34418029a98fcbcf86be8743988267d0de1
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-18 11:51:24 -0700

    Tenzies setup

---
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/14 Tenzies Project intro/src/main.jsx
@@ -0,0 +1,17 @@
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

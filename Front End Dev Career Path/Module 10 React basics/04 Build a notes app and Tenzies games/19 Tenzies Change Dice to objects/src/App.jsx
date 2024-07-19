8c29bddd8055fda0f24f7d625c7051dee0217258
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-19 09:33:43 -0700

    Make die an object

--- Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/19 Tenzies Change Dice to objects/src/App.jsx
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/19 Tenzies Change Dice to objects/src/App.jsx
@@ -1,33 +1,38 @@
 import React from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 
 export default function App() {
-/**
- * Challenge: Create a `Roll Dice` button that will re-roll
- * all 10 dice
- * 
- * Clicking the button should generate a new array of numbers
- * and set the `dice` state to that new array (thus re-rendering
- * the array to the page)
- */
+    /*
+    * Challenge: Update the array of numbers in state to be
+    * an array of objects instead. Each object should look like:
+    * { value: <random number>, isHeld: false }
+    * 
+    * Making this change will break parts of our code, so make
+    * sure to update things so we're back to a working state
+    */
 
     const [dice, setDice] = React.useState(allNewDice())
     
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
     
-    const diceElements = dice.map(die => <Die value={die} />)
-    
     function rollDice() {
         setDice(allNewDice())
     }
-        
+    
+    const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+    
     return (
         <main>
             <div className="dice-container">

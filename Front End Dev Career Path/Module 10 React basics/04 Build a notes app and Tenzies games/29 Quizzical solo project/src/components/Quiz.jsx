fb767240319a89a6444cf8fd229819b4436653e2
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-25 11:34:52 -0700

    Start completed

---
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/components/Quiz.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Quiz() {
+    return(
+        <div className='quiz-container'>
+            <h2>Test</h2>
+        </div>
+    )
+}
---
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/components/Start.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Start(props) {
+    return(
+        <div className='start'>
+            <h1 className='start--title'>Quizzical</h1>
+            <h2 className='start--subtitle'>A Movie Trivia App</h2>
+            <button 
+                className='start--btn'
+                onClick={props.startQuiz}
+            >
+                Start Quiz
+            </button>
+        </div>
+    )
+}

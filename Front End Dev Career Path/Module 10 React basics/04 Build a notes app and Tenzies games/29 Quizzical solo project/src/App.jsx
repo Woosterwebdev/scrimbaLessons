37189c6cf3bae214a99c8a6b4f9c84281f46b558
Christopher Wooster <woosterwebdev@gmail.com>
2024-07-26 11:45:47 -0700

    Refactoring app

--- Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/App.jsx
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/App.jsx
@@ -1,97 +1,98 @@
+/* Hard coded temporarily due to api constrains */
+const data = [
+  {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Entertainment: Film",
+      "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+          "True"
+      ]
+  },
+  {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Entertainment: Film",
+      "question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+          "True"
+      ]
+  },
+  {
+      "type": "multiple",
+      "difficulty": "easy",
+      "category": "Entertainment: Film",
+      "question": "This movie contains the quote, &quot;I feel the need ... the need for speed!&quot;",
+      "correct_answer": "Top Gun",
+      "incorrect_answers": [
+          "Days of Thunder",
+          "The Color of Money",
+          "Cocktail"
+      ]
+  },
+  {
+      "type": "multiple",
+      "difficulty": "easy",
+      "category": "Entertainment: Film",
+      "question": "Who directed the Kill Bill movies?",
+      "correct_answer": "Quentin Tarantino",
+      "incorrect_answers": [
+          "Arnold Schwarzenegger",
+          "David Lean",
+          "Stanley Kubrick"
+      ]
+  },
+  {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Entertainment: Film",
+      "question": "In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+          "False"
+      ]
+  }
+]
 import React from 'react'
-import nanoid from 'nanoid'
+import { nanoid } from 'nanoid'
 import Start from './components/Start'
 import Quiz from './components/Quiz'
+import Question from './components/Question'
 
 export default function App() {
-  const [start, setStart] = React.useState(true)
-  const [quiz, setQuiz] = React.useState([data])
+  const [start, setStart] = React.useState(false)
+  const [quiz, setQuiz] = React.useState(data)
   const [correctAnswers, setCorrectAnswers] = React.useState([])
-
-
 
   function startQuiz() {
     setStart(prevStart => !prevStart)
   }
 
-  let data = [
-    {
-        "type": "boolean",
-        "difficulty": "easy",
-        "category": "Entertainment: Film",
-        "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "type": "boolean",
-        "difficulty": "easy",
-        "category": "Entertainment: Film",
-        "question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "type": "multiple",
-        "difficulty": "easy",
-        "category": "Entertainment: Film",
-        "question": "This movie contains the quote, &quot;I feel the need ... the need for speed!&quot;",
-        "correct_answer": "Top Gun",
-        "incorrect_answers": [
-            "Days of Thunder",
-            "The Color of Money",
-            "Cocktail"
-        ]
-    },
-    {
-        "type": "multiple",
-        "difficulty": "easy",
-        "category": "Entertainment: Film",
-        "question": "Who directed the Kill Bill movies?",
-        "correct_answer": "Quentin Tarantino",
-        "incorrect_answers": [
-            "Arnold Schwarzenegger",
-            "David Lean",
-            "Stanley Kubrick"
-        ]
-    },
-    {
-        "type": "boolean",
-        "difficulty": "easy",
-        "category": "Entertainment: Film",
-        "question": "In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    }
-]
-  // React.useEffect(() => {
-  //   fetch('https://opentdb.com/api.php?amount=5&category=11&difficulty=easy')
-  //     .then(res => res.json())
-  //     .then(data => setQuiz(data.results))
-  // }, [start])
-
-  const questionElements = quiz.map(question => {
-    
+  const questionElements = quiz.map(object => {
+    let answers = [...object.incorrect_answers, object.correct_answer]
+    return(
+      <Question
+        key={object.id}
+        question={object.question}
+        answers={answers}
+      />
+    )
   })
-  
+    
   if (start) {
     return(
       <Start
-        startQuiz={() => startQuiz()}
+      startQuiz={() => startQuiz()}
       />
     )
   } else {
     return(
-      <Quiz 
-        key={nanoid}
-        quiz={quiz}
-      />
+      <div className='quiz-container'>
+        {questionElements}
+        <button className='btn check'>Check Answers</button>
+      </div>
     )
   }  
 }
@@ -99,22 +100,38 @@ export default function App() {
 
 const [start, setStart] = React.useState(true)
 
-  function startQuiz() {
-    setStart(prevStart => !prevStart)
+function startQuiz() {
+  setStart(prevStart => !prevStart)
   }
-
-if (start) {
-  return(
-    <Start
-      startQuiz={() => startQuiz()}
-    />
-  )
-} else {
-  return(
-    <Quiz />
-  )
-}
   
+  if (start) {
+    return(
+      <Start
+      startQuiz={() => startQuiz()}
+      />
+      )
+      } else {
+        return(
+      <Quiz />
+      )
+      }
+      
       question={data.question}
       correctAnswer={data.correct_answer}
-      incorrectAnswers={data.incorrect_answers}*/
+      incorrectAnswers={data.incorrect_answers}
+      
+      
+      
+      React.useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&category=11&difficulty=easy')
+          .then(res => {
+                if(!res.ok) {
+                  throw Error("Something went wrong")
+                  }
+                res.json()
+              })
+          .then(data => setQuiz({...data.results), id: {nanoid}})
+      }, [start])
+
+
+*/
--- Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/components/Question.jsx
+++ Front End Dev Career Path/Module 10 React basics/04 Build a notes app and Tenzies games/29 Quizzical solo project/src/components/Question.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import{ decode } from 'he'
 
-export default function Question() {
+export default function Question(props) {
+    let shuffleAnswers = props.answers.sort()
+    if (shuffleAnswers.length < 3){
+            shuffleAnswers.sort().reverse()
+        }
+
+    let answers = shuffleAnswers.map(answer => {
+        return(
+            <label className='answer'>{decode(answer)}<input type='radio' name='answer'></input></label>
+        )
+    })
     return(
         <div className='question-container'>
             <form>
-                <legend>Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</legend>
+                <legend>{decode(props.question)}</legend>
                 <div className='answer-container'>
-                    <label className='answer'>Cabbage Patch Kids<input type='radio' name='answer'></input></label>
-                    <label className='answer'>Transformers<input type='radio' name='answer'></input></label>
-                    <label className='answer'>Care Bears<input type='radio' name='answer'></input></label>
-                    <label className='answer'>Rubik’s Cube<input type='radio' name='answer'></input></label>
+                    {answers}
                 </div>
             </form>
             <hr />
         </div>
     )
 }
+/* <label className='answer'>Cabbage Patch Kids<input type='radio' name='answer'></input></label>
+<label className='answer'>Transformers<input type='radio' name='answer'></input></label>
+<label className='answer'>Care Bears<input type='radio' name='answer'></input></label>
+<label className='answer'>Rubik’s Cube<input type='radio' name='answer'></input></label> */

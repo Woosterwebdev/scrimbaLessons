61cded7a6490e2ee516e13b6e57feb69e551a16c
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-02 09:17:23 -0700

    spread and destructuring

--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/04 Challenge add onClick event listener/Button.js
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/04 Challenge add onClick event listener/Button.js
@@ -2,6 +2,21 @@ import React from "react"
 
 export default function Button(props) {
     return (
-        <button onClick={props.onClick}>{props.children}</button>
+        <button onClick={props.onClick}>
+            {props.children}
+        </button>
     )
 }
+
+/* destructuring or using spread is also an option
+    the prop paced in MUST use the button attributes.
+    ie "onClick", "sytle", etc
+
+export default function Button({children, ...rest}) {
+    return (
+        <button {...rest}>
+            {children}
+        </button>
+    )
+}
+*/

8e86a620947b86baef5da89b290d7bbeb96830e4
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-02 08:36:22 -0700

    Early children challenge

--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/02 Challenge Button with Children/index.js
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/02 Challenge Button with Children/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from "./Button"
 
+/**
+ * Challenge: Make the Button accept and display children.
+ * 
+ * Change the text to say "Buy now!" so it's less boring 🥱
+ */
+
 function App() {
   return (
     <main>
-      <Button text="Click here" />
-      {/* The concept of "children" is found everywhere in HTML! */}
-      
-      
+      <Button>Buy now!</Button>
     </main>
   )
 }

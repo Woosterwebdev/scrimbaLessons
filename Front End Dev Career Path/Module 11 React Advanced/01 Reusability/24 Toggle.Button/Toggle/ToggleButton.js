7c90de1dbf9ed7101607db7ddb81053aa64202cd
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-14 09:18:47 -0700

    ToggleButton

---
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/24 Toggle.Button/Toggle/ToggleButton.js
@@ -0,0 +1,21 @@
+/**
+ * Challenge:
+ * 
+ * 1. Create a ToggleButton component
+ * 2. Receive the `toggle` function from context (2 parts!)
+ * 3. Render a div with an onClick that calls `toggle` 
+ *    - also renders children
+ */
+
+import React from 'react'
+import { ToggleContext } from './Toggle'
+
+export default function ToggleButton({ children }) {
+    const { toggle } = React.useContext(ToggleContext)
+    
+    return(
+        <div onClick={toggle}>
+            {children}
+        </div>
+    )
+}

a2c440fdc1b238ecf2a87ae2ea215e863d6021d0
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-19 09:24:44 -0700

    update toggle.js and index.js

--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/components/Toggle/Toggle.js
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/components/Toggle/Toggle.js
@@ -1,4 +1,5 @@
 import React from "react"
+import useEffectOnUpdate from "../../hooks/useEffectOnUpdate"
 
 const ToggleContext = React.createContext()
 
@@ -8,6 +9,17 @@ export default function Toggle({ children, onToggle = () => {}}) {
     function toggle() {
         setOn(prevOn => !prevOn)
     }
+
+    /**
+     * Challenge: use our new, shiny custom hook
+     * to set up the effect again. You'll know it's
+     * working when the console log runs after clicking
+     * the box, but not on the initial render.
+     */
+
+    useEffectOnUpdate(onToggle, [on]) 
+    
+    // if multiple functions () => {onToggle()}
 
     return (
         <ToggleContext.Provider value={{ on, toggle }}>

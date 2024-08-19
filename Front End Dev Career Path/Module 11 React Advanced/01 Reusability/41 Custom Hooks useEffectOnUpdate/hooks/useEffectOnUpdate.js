00ea03629686774c4c79df1734e5c9e3581e3352
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-19 09:02:24 -0700

    Custom Hooks useEffectOnUpdate

---
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/hooks/useEffectOnUpdate.js
@@ -0,0 +1,9 @@
+function useEffectOnUpdate() {
+    
+}
+
+/**
+ * Mini-quiz:
+ * What are the 2 parameters in React.useEffect?
+ * The function or action taken, and the dependency (when the function/action show occur)
+ */

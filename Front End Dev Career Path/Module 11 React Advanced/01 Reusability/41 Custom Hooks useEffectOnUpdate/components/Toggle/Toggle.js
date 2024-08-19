76e746727e91970b52bfc2ab7bf6804bf04444f1
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-19 09:15:56 -0700

    update parameters

--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/components/Toggle/Toggle.js
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/components/Toggle/Toggle.js
@@ -4,20 +4,10 @@ const ToggleContext = React.createContext()
 
 export default function Toggle({ children, onToggle = () => {}}) {
     const [on, setOn] = React.useState(false)
-    const firstRender = React.useRef(true)
-    
 
     function toggle() {
         setOn(prevOn => !prevOn)
     }
-
-    React.useEffect(() => {
-        if (firstRender.current) {
-            firstRender.current = false
-        } else {
-            onToggle()
-        }
-    }, [on])
 
     return (
         <ToggleContext.Provider value={{ on, toggle }}>
--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/hooks/useEffectOnUpdate.js
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/41 Custom Hooks useEffectOnUpdate/hooks/useEffectOnUpdate.js
@@ -1,9 +1,26 @@
-function useEffectOnUpdate() {
-    
+import React from "react"
+
+export default function useEffectOnUpdate(effectFunction, deps) {
+    const firstRender = React.useRef(true)
+
+    React.useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false
+        } else {
+            effectFunction()
+        }
+    }, deps)
 }
 
 /**
  * Mini-quiz:
  * What are the 2 parameters in React.useEffect?
- * The function or action taken, and the dependency (when the function/action show occur)
+ * 1 callback function
+ * 2 dependencies (when the function should occur)
+ * 
+ *  * Mini-challenge:
+ * Update our custom hook function to accept
+ * the same parameters we talked about above
+ * Update the code to use our hook's parameters
+ * instead of the values from the Toggle component
  */

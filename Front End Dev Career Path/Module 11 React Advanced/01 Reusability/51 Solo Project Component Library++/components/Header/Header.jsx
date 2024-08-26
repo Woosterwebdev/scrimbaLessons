6b8bbbabd1307ef98aaad2d2a5c6e835d5129fa5
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-26 11:48:15 -0700

    Docs wip

---
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/51 Solo Project Component Library++/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return(
+        <div className="header">
+            <img src="./public/react-icon.png" />
+            <h1 className="title m-0">React Component Library</h1>
+        </div>
+    )
+}
--- Front End Dev Career Path/Module 11 React Advanced/01 Reusability/51 Solo Project Component Library++/index.jsx
+++ Front End Dev Career Path/Module 11 React Advanced/01 Reusability/51 Solo Project Component Library++/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header/Header"
 import Menu from "./components/Menu/index"
 import Star from "./components/Star"
 import Badge from "./components/Badge/Badge"
@@ -10,12 +11,70 @@ import Testimonial from "./components/Testimonial/index"
 
 function App() {
   return (
-    <Testimonial logo="logo">
-      <Testimonial.Quote>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
-      </Testimonial.Quote>
-      <Testimonial.Author author="May Andersons" role="Workcation, CTO" />
-    </Testimonial>
+    <>
+      <Header />
+      <section>
+        <h2>Badges</h2>
+          <h3>Square</h3>
+            <div className="badge-examples">
+              <Badge style="square" variant="success">Success</Badge>
+              <Badge style="square" variant="warning">Warning</Badge>
+              <Badge style="square" variant="danger">Danger</Badge>
+              <Badge style="square" variant="primary">Primary</Badge>
+              <Badge style="square" variant="indigo">Indigo</Badge>
+              <Badge style="square" variant="purple">Purple</Badge>
+              <Badge style="square" variant="pink">Pink</Badge>
+            </div>
+          <h3>Pill</h3>
+            <div className="badge-examples">
+              <Badge style="pill" variant="success">Success</Badge>
+              <Badge style="pill" variant="warning">Warning</Badge>
+              <Badge style="pill" variant="danger">Danger</Badge>
+              <Badge style="pill" variant="primary">Primary</Badge>
+              <Badge style="pill" variant="indigo">Indigo</Badge>
+              <Badge style="pill" variant="purple">Purple</Badge>
+              <Badge style="pill" variant="pink">Pink</Badge>
+            </div>
+          <h3>Syntax</h3>
+            <div className="syntax">
+              <p><code>{`<Badge style="pill" variant="success">Success</Badge>`}</code></p>
+            </div>
+      </section>
+      <section>
+        <h2>Banners</h2>
+          <div className="banner-examples">
+            <Banner variant="success">
+              <Banner.Title>Congratulations!</Banner.Title>
+              <Banner.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Content>
+            </Banner>
+            <Banner variant="warning">
+              <Banner.Title>Warning!</Banner.Title>
+              <Banner.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Content>
+            </Banner>
+            <Banner variant="danger">
+              <Banner.Title>Danger!</Banner.Title>
+              <Banner.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Content>
+            </Banner>
+            <Banner variant="primary">
+              <Banner.Title>Primary</Banner.Title>
+              <Banner.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Content>
+            </Banner>
+          </div>
+          <h3>Syntax</h3>
+            <div className="syntax">
+              <p><code>{`<Banner variant="primary">`}</code></p>
+              <p><code>{`<Banner.Title>Primary</Banner.Title>`}</code></p>
+              <p><code>{`<Banner.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Content>`}</code></p>
+              <p><code>{`</Banner>`}</code></p>
+            </div>
+      </section>
+      <section>
+        <h2>Cards</h2>
+      </section>
+      <footer>
+        <p>&copy; 2024</p>
+      </footer>
+    </>
   )
 }
 
@@ -23,7 +82,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 
 /*
-      <Testimonial.Logo logo="logo" />
+      <header>
+        <h1>Wooster's React Component Library</h1>
+      </header>
     <Badge style="square" variant="">Badge</Badge>
     ***
     <Banner style="" variant="primary">
@@ -45,4 +106,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(<App />);
       </TestimonialWithImg.Quote>
       <TestimonialWithImg.Author author="May Andersons" role="Workcation, CTO" />
     </TestimonialWithImg>
+    ***
+      <Testimonial logo="logo">
+        <Testimonial.Quote>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
+        </Testimonial.Quote>
+        <Testimonial.Author author="May Andersons" role="Workcation, CTO" />
+      </Testimonial>
 */

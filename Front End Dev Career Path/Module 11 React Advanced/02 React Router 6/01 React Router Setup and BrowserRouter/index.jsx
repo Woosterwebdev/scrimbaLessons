bde40f3d012f245c00f851aaf1b40dc04926b889
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-29 07:59:14 -0700

    BrowserRouter Setup

---
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/01 React Router Setup and BrowserRouter/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom" // BrowserRouter is a .Provider
+
+function App() {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+//App needs to be nested in BrowserRouter to use the tools

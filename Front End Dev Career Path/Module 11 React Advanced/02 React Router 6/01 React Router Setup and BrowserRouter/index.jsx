67ec9e485844324a4575380cc4f376bdd8598109
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-29 08:28:26 -0700

    React Routes, path, and element

--- Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/01 React Router Setup and BrowserRouter/index.jsx
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/01 React Router Setup and BrowserRouter/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom" // BrowserRouter is a .Provider
+import { BrowserRouter } from "react-router-dom" // BrowserRouter is a context Provider
 
 function App() {
   return (
---
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/03 Route, path, & element/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+function About() {
+  return(
+    <h1>About React Routeer!</h1>
+  )
+}
+
+/**
+ * Challenge:
+ * 1. Create an "About" component (just render another h1 that says
+ *    "About page" or something more interesting of your choosing).
+ * 2. Create a new Route to render the About component when the path
+ *    is /about
+ */
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);

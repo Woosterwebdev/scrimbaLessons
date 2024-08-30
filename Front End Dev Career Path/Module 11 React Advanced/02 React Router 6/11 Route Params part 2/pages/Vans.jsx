cbbb10ddf8582c721ce715092168036d752e98a3
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-30 09:56:05 -0700

    Clean up

--- Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/11 Route Params part 2/pages/Vans.jsx
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/11 Route Params part 2/pages/Vans.jsx
@@ -18,11 +18,11 @@ export default function Vans() {
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
             </Link>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
         </div>

bce5128a6ae1566d2a0d39820e856ab57b404a81
Christopher Wooster <woosterwebdev@gmail.com>
2024-08-30 10:08:29 -0700

    accessibility improvement

--- Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/11 Route Params part 2/pages/Vans.jsx
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/11 Route Params part 2/pages/Vans.jsx
@@ -20,7 +20,7 @@ export default function Vans() {
             <Link to={`/vans/${van.id}`} 
             aria-label={`View details for ${van.name}, 
                         priced at $${van.price} per day`}>
-                <img src={van.imageUrl} />
+                <img src={van.imageUrl} alt={`Image of ${van.name}`} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
                     <p>${van.price}<span>/day</span></p>

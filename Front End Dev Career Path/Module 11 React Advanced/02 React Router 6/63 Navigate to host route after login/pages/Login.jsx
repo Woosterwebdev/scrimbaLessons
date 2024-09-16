a82ef35d037b9c881d84c456ba57c5e3cab96872
Christopher Wooster <woosterwebdev@gmail.com>
2024-09-16 12:15:36 -0700

    Navigate to host route after login

---
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/63 Navigate to host route after login/pages/Login.jsx
@@ -0,0 +1,83 @@
+import React from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import { loginUser } from "../api"
+
+/**
+ * Challenge: read up on the useNavigate hook from the
+ * docs and implement it in the VanLife app. When the user
+ * successfully logs in, they should be redirected to the 
+ * /host route.
+ */
+
+export default function Login() {
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+                console.log(data)
+                setError(null)
+                navigate("/host")
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    return (
+        <div className="login-container">
+            {
+                location.state ?.message &&
+                    <h3 className="login-error">{location.state.message}</h3>
+            }
+            <h1>Sign in to your account</h1>
+            {
+                error ?.message &&
+                    <h3 className="login-error">{error.message}</h3>
+            }
+
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    name="email"
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Email address"
+                    value={loginFormData.email}
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    type="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                />
+                <button
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
+            </form>
+        </div>
+    )
+
+}

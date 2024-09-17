7cd253119d5f50c3d9e0c70a9a1a15c046d12d40
Christopher Wooster <woosterwebdev@gmail.com>
2024-09-17 08:21:51 -0700

    refactoring getVans and writing getVan

--- Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/67 Cloud Firestore Setup/api.js
+++ Front End Dev Career Path/Module 11 React Advanced/02 React Router 6/67 Cloud Firestore Setup/api.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore/lite"
+import { getFirestore, collection, doc, getDocs, getDoc } from "firebase/firestore/lite"
 
 const firebaseConfig = {
     apiKey: `${process.env.APIKEY}`,
@@ -15,22 +15,37 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 // Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
 
-
-
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    const van = snapshot.data()
+    return van
+}
+
+
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"

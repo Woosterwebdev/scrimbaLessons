99f79f134f5dc5f6768cb2a594608fca4bc57422
Christopher Wooster <woosterwebdev@gmail.com>
2024-09-17 12:28:26 -0700

    React lazy and suspense

--- Front End Dev Career Path/Module 11 React Advanced/03 Performance/05 Code Splitting lazy suspense/index.jsx
+++ Front End Dev Career Path/Module 11 React Advanced/03 Performance/05 Code Splitting lazy suspense/index.jsx
@@ -1,6 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import ProductsList from "./ProductsList"
+// import ProductsList from "./ProductsList"
+
+/**
+ * Challenge: Add back the dynamic import of the ProductsList
+ * component.
+ * 
+ * 1. Use `React.lazy()` to create a suspending component
+ *    that will only render when needed
+ * 2. Use the dynamic `import()` as the return value inside of
+ *    `React.lazy()`
+ * 3. Use the `<React.Suspense>` component will a `fallback`
+ *    prop to give React something to render in the meantime
+ *    while it's downloading all the code for the suspended
+ *    component.
+ */
+
+const ProductsList = React.lazy(() => {
+  return import("./ProductsList")
+})
 
 function App() {
   const [count, setCount] = React.useState(0)
@@ -33,9 +51,11 @@ function App() {
             </button>
       <br />
       <br />
-      <div className="products-list">
-        {showProducts && <ProductsList />}
-      </div>
+      <React.Suspense fallback={<h2>Loading...</h2>}>
+        <div className="products-list">
+          {showProducts && <ProductsList />}
+        </div>
+      </React.Suspense>
     </>
   )
 }

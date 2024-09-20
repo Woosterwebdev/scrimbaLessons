e4540ba3c2e1482a70dad34e304c723aba3c031c
Christopher Wooster <woosterwebdev@gmail.com>
2024-09-20 09:37:58 -0700

    Challenge 10

---
+++ Front End Dev Career Path/Module 12 Getting hired/JavaScript Challenges/Challenge 10/index.js
@@ -0,0 +1,34 @@
+/*  
+Grandpa's hand isn't as steady as it used to be. You finally convinced him
+to start using a password manager, but he accidentally typed and saved his
+password with a bunch of extra characters. Help him recover his password by 
+removing all the duplicate characters. 
+
+Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
+
+Example input: "aabbccb1212"
+Example output: "abc12"
+*/ 
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
+ 
+function removeDupeChars(chars){
+    const newstr = [...new Set(chars.split(""))].join("")
+    return newstr
+}
+
+//brute force
+/*
+function removeDupeChars(chars){
+    let dupesRemoved = '';
+    for(let i = 0; i < chars.length; i++){
+        if(!dupesRemoved.includes(chars[i])){
+            dupesRemoved += chars[i];
+        }
+    }
+    return dupesRemoved;
+}
+*/
+
+console.log(removeDupeChars(password));

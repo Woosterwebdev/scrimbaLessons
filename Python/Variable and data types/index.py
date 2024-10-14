87a45fb4c0ce09c846c43fb878edc95f269d7920
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-14 08:10:14 -0700

    Pythons lessons

---
+++ Python/Variable and data types/index.py
@@ -0,0 +1,9 @@
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+
+tool_name="Hammer"
+tool_price=3.99
+tool_quantity=7
+
+print(str(tool_name) + " " + str(tool_price) + " " + str(tool_quantity))

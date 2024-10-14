b62fef632b197d4c32fbe8080033d7bd661262a7
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-14 09:49:41 -0700

    Python lessons

---
+++ Python/5- Strings- Find replace string formatting/index.py
@@ -0,0 +1,9 @@
+name='TERRY'
+color = 'RED'
+msg = '[' + name + '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+print(msg)
+print(msg1)
+
+
+

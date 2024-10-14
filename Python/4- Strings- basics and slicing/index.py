9d7d6ddf52f39567aef979b04d09b54b9e33cc75
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-14 09:40:35 -0700

    Python lessons

---
+++ Python/4- Strings- basics and slicing/index.py
@@ -0,0 +1,7 @@
+msg='welcome to Python 101: Strings'
+  
+print(msg)
+str1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]
+
+print(str1.title())
+print(str1.title()[::-1])

647fe4f27c8aa05599e04834add3771e5a4b198b
Christopher Wooster <woosterwebdev@gmail.com>
2024-10-21 13:57:30 -0700

    Tuples intro

---
+++ Python/10- Tuples/index.py
@@ -0,0 +1,8 @@
+#Tuples - faster Lists you can't change
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+print(friends[2:4])
+print(friends_tuple[2:4])
+
+# Tuples are immutable and used when you have code that you 
+# don't want to ever change.
